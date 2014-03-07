0d363f8b4e8fe9fe5e597fb094a816310a1c6782
Parthiv Mohan <parthivm@berkeley.edu>
2014-03-06 16:05:53 -0800

    added correct chord generation

---
+++ chordquery.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+   $(".ace").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer1").fadeIn(1000);
+   });
+   $(".bdf").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer2").fadeIn(1000);
+   }); 
+   $(".ceg").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer3").fadeIn(1000);
+   });
+   $(".dfa").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer4").fadeIn(1000);
+   }); 
+   $(".egb").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer5").fadeIn(1000);
+   });
+   $(".fac").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer6").fadeIn(1000);
+   });
+   $(".gbd").click(function() {
+       $("#notecontainer").fadeOut(500);
+       $("#chordcontainer7").fadeIn(1000);
+   });
+});
+
