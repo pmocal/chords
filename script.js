0df7a30d97db62d02e67501e5a71af5536696f9c
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-02-20 11:56:09 -0500

    updated

--- script.js
+++ script.js
@@ -64,6 +64,47 @@ function playChord(keys, delay) {
 	MIDI.chordOn(0, midiNotes, 127, delay);
 }
 
+MIDI.loadPlugin = function(conf) {
+	if (typeof(conf) === "function") conf = { callback: conf };
+	/// Get the instrument name.
+	var instruments = conf.instruments || conf.instrument || "acoustic_grand_piano";
+	if (typeof(instruments) !== "object") instruments = [ instruments ];
+	instruments.map(function(data) {
+		if (typeof(data) === "number") data = MIDI.GeneralMIDI.byId[data];
+		return data;		
+	});
+	///
+	MIDI.soundfontUrl = conf.soundfontUrl || MIDI.soundfontUrl || "./soundfont/";
+	/// Detect the best type of audio to use.
+	MIDI.audioDetect(function(types) {
+		var type = "";
+		// use the most appropriate plugin if not specified
+		if (typeof(type) === 'undefined') {
+			if (plugins[window.location.hash]) {
+				type = window.location.hash.substr(1);
+			} else { //
+				type = "";
+			}
+		}
+		if (type === "") {
+			if (navigator.requestMIDIAccess) {
+				type = "webmidi";
+			} else if (window.webkitAudioContext) { // Chrome
+				type = "webaudio";
+			} else if (window.Audio) { // Firefox
+				type = "audiotag";
+			} else { // Internet Explorer
+				type = "flash";
+			}
+		}
+		if (!connect[type]) return;
+		// use audio/ogg when supported
+		var filetype = types["audio/ogg"] ? "ogg" : "mp3";
+		// load the specified plugin
+		connect[type](filetype, instruments, conf.callback);
+	});
+};
+
 function playMidi(note){
 	var note1;
 	var note2;
